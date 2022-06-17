fdf41b2fe7801214171ebd26575ee400790c0a57
Lier0102 <75836412+Lier0102@users.noreply.github.com>
2022-06-17 23:43:15 +0900

    seconde

--- index.ts
+++ index.ts
@@ -13,6 +13,11 @@ const client = new DJS.Client({
 client.on('ready', () => {
     console.log('봇이 준비되었습니다.')
 
+    let handler = require('./command-handler')
+    if (handler.default) handler = handler.default
+
+    handler(client)
+
     new WOK(client, {
         commandsDir: path.join(__dirname, 'commands'),
         typeScript: true,
