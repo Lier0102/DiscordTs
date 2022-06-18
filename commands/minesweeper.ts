3672ce2fa79e7864967eae78a3c0060eab1ed81f
Lier0102 <75836412+Lier0102@users.noreply.github.com>
2022-06-19 00:39:33 +0900

    added minesweeper.ts

---
+++ commands/minesweeper.ts
@@ -0,0 +1,9 @@
+import { ICommand } from "wokcommands";
+import { DiscordMessage, DiscordUser, DiscordEmbed, DiscordInteraction, DiscordMessageReactionAdd, DiscordMessageActionRow, DiscordMessageButton, DiscordButtonStyle, DiscordSelectMenu, DiscordSelectOption } from 'discord-minimal';
+
+const WIDTH = 9;
+const HEIGHT = 8;
+
+export default {
+    
+} as ICommand
