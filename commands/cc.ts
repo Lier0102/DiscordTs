7ddb6a841738e1d112c478db4bbdf229ffe0ccbc
Lier0102 <75836412+Lier0102@users.noreply.github.com>
2022-06-21 08:22:27 +0900

    Update cc.ts
    arula

--- commands/cc.ts
+++ commands/cc.ts
@@ -1,16 +1,18 @@
 import { ICommand } from "wokcommands";
 
 export default {
-    category: 'Moderation',
+    category: 'Moderation', // 도움말에 표시될 카테고리 명
     description: '여러개의 메세지를 한 번에 삭제합니다.',
-
+    // 도움말에 표시될 설명란
     permissions: ['ADMINISTRATOR'],
+    // 필요권한, requirerole: true로 해도 되지만
+    // 예상치 못한 오류로 임시로 저 구문을 쓰게 됨
 
-    maxArgs: 1,
-    expectedArgs: '[개수]',
+    maxArgs: 1,// 최대로 필요한 인자(arguments)
+    expectedArgs: '[개수]',// 필요한 인자의 타입
 
-    slash: 'both',
-    testOnly: false,
+    slash: 'both',// 앱 커맨드와 봇 커맨드 동시 지원
+    testOnly: false,// 테스트를 완료해서 테스트 값을 끔
 
     callback: async ({ message, interaction, channel, args }) => {
         // const amount = parseInt(args.shift()!)
